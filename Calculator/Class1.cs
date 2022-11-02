5b9f8d41c04f786bc9a45997b6b4851134aadfb2
PhoeniXfire208 <subi9999@mail.ru>
2022-11-02 14:56:14 +0800

    Добавьте файлы проекта.

---
+++ Calculator/Class1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public enum MeasureType {ml,l,m3,bar};
+
+    public class Length
+    {
+        private double value;
+        private MeasureType type;
+
+        public Length(double value, MeasureType type)
+        {
+            this.value = value;
+            this.type = type;
+        }
+
+        public static Length operator +(Length instance, double number)
+        {
+            var newValue = instance.value + number;
+
+            var length = new Length(newValue, instance.type);
+
+            return length;
+        }
+
+        public static Length operator +(double number, Length instance)
+        {
+            return instance + number;
+        }
+
+        public static Length operator *(Length instance, double number)
+        {
+            return new Length(instance.value * number, instance.type); ;
+        }
+
+        public static Length operator *(double number, Length instance)
+        {
+            return instance * number;
+        }
+
+        // вычитание
+        public static Length operator -(Length instance, double number)
+        {
+            return new Length(instance.value - number, instance.type); ;
+        }
+
+        public static Length operator -(double number, Length instance)
+        {
+            return instance - number;
+        }
+
+        // деление
+        public static Length operator /(Length instance, double number)
+        {
+            return new Length(instance.value / number, instance.type); ;
+        }
+
+        public static Length operator /(double number, Length instance)
+        {
+            return instance / number;
+        }
+
+        public Length To(MeasureType newType)
+        {
+            
+            var newValue = this.value;
+            
+            if (this.type == MeasureType.ml)
+            {
+                switch (newType)
+                {
+                    case MeasureType.ml:
+                        newValue = this.value;
+                        break;
+                    // если в литры
+                    case MeasureType.l:
+                        newValue = this.value / 1000;
+                        break;
+                    // если в м3
+                    case MeasureType.m3:
+                        newValue = this.value / 1000000;
+                        break;
+                    // если в баррели
+                    case MeasureType.bar:
+                        newValue = this.value / 158987.294928;
+                        break;
+                }
+            }
+
+            else if (newType == MeasureType.ml)
+            {
+                switch (this.type)
+                {
+                    case MeasureType.ml:
+                        newValue = this.value;
+                        break;
+                    case MeasureType.l:
+                        newValue = this.value * 1000;
+                        break;
+                    case MeasureType.m3:
+                        newValue = this.value * 1000000;
+                        break;
+                    case MeasureType.bar:
+                        newValue = this.value * 158987.294928;
+                        break;
+                }
+            }
+
+            else
+            {
+                newValue = this.To(MeasureType.ml).To(newType).value;
+            }
+            return new Length(newValue, newType);
+        }
+
+        public string Verbose()
+        {
+            string typeVerbose = "";
+            
+            switch (type)
+            {
+                case MeasureType.ml:
+                    typeVerbose = "Мл.";
+                    break;
+                case MeasureType.l:
+                    typeVerbose = "Л.";
+                    break;
+                case MeasureType.m3:
+                    typeVerbose = "м3.";
+                    break;
+                case MeasureType.bar:
+                    typeVerbose = "Баррел.";
+                    break;
+            }
+            return $"{this.value} {typeVerbose}";
+        }
+
+        public static Length operator +(Length instance1, Length instance2)
+        {
+            return instance1 + instance2.To(instance1.type).value;
+        }
+
+        public static Length operator -(Length instance1, Length instance2)
+        {
+            return instance1 - instance2.To(instance1.type).value;
+        }
+    }
+}
---
+++ CalculatorTests/LengthTests.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests
+{
+    [TestClass()]
+    public class LengthTests
+    {
+        [TestMethod()]
+        public void VerboseTest()
+        {
+            var length = new Length(10, MeasureType.ml);
+            Assert.AreEqual("10 Мл.", length.Verbose());
+
+            length = new Length(10, MeasureType.l);
+            Assert.AreEqual("10 Л.", length.Verbose());
+
+            length = new Length(10, MeasureType.m3);
+            Assert.AreEqual("10 м3.", length.Verbose());
+
+            length = new Length(10, MeasureType.bar);
+            Assert.AreEqual("10 Баррел.", length.Verbose());
+        }
+        
+        [TestMethod()]
+        public void AddNumberTest()
+        {
+            var length= new Length(1, MeasureType.ml);
+            length = length + 4.25;
+            Assert.AreEqual("5.25 Мл.", length.Verbose());
+        }
+
+        [TestMethod()]
+        public void SubNumberTest()
+        {
+            var length = new Length(3, MeasureType.ml);
+            length = length - 1.75;
+            Assert.AreEqual("1.25 Мл.", length.Verbose());
+        }
+
+        [TestMethod()]
+        public void MulByNumberTest()
+        {
+            var length = new Length(3, MeasureType.ml);
+            length = length * 3;
+            Assert.AreEqual("9 Мл.", length.Verbose());
+        }
+
+        [TestMethod()]
+        public void DivByNumberTest()
+        {
+            var length = new Length(3, MeasureType.ml);
+            length = length / 3;
+            Assert.AreEqual("1 Мл.", length.Verbose());
+        }
+        
+        [TestMethod()]
+        public void MeterToAnyTest()
+        {
+            Length length;
+
+            length = new Length(1000, MeasureType.ml);
+            Assert.AreEqual("1 Л.", length.To(MeasureType.l).Verbose());
+
+            length = new Length(1000000, MeasureType.ml);
+            Assert.AreEqual("1 м3.", length.To(MeasureType.m3).Verbose());
+
+            length = new Length(158987.294928, MeasureType.ml);
+            Assert.AreEqual("1 Баррел.", length.To(MeasureType.bar).Verbose());
+        }
+
+        [TestMethod()]
+        public void AddSubKmMetersTest()
+        {
+            var ml = new Length(100, MeasureType.ml);
+            var l = new Length(1, MeasureType.l);
+
+            Assert.AreEqual("1100 Мл.", (ml + l).Verbose());
+            Assert.AreEqual("1.1 Л.", (l + ml).Verbose());
+
+            Assert.AreEqual("0.9 Л.", (l - ml).Verbose());
+            Assert.AreEqual("-900 Мл.", (ml - l).Verbose());
+        }
+    }
+}
